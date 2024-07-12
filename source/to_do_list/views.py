09af16a02a402dd3a78b75313701fc8615d15c4c
ABM1204 <bekabek13@bk.ru>
2024-07-12 19:40:23 +0600

    Создал представление списка задача, создал шаблон

--- source/to_do_list/views.py
+++ source/to_do_list/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
+from to_do_list.models import Task
+
+
+class TaskListView(View):
+    def get(self, request, *args, **kwargs):
+        tasks = Task.objects.all()
+        return render(request, 'index.html', {'tasks': tasks})
 
 
