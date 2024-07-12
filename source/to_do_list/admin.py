126dc832e45fa6f2bbcfb10e218a0a1704bcc6c4
ABM1204 <bekabek13@bk.ru>
2024-07-12 19:27:16 +0600

    Зарегал модели в админке, зарегал адина, через админ-панель добавил записи, создал для разных записей разные файлы фикстур.

--- source/to_do_list/admin.py
+++ source/to_do_list/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Type, Task, Status
 
-# Register your models here.
+admin.site.register(Type)
+admin.site.register(Task)
+admin.site.register(Status)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'status', 'type', 'created_at', 'updated_at')
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('name')
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('name')
