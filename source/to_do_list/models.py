be6f693215609856385c44f08ad1d26a6ef8e3b1
ABM1204 <bekabek13@bk.ru>
2024-07-15 18:58:57 +0600

    Изменил проект по требованиям тз

--- source/to_do_list/models.py
+++ source/to_do_list/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
-class Type(models.Model):
+from django.db import models
+from to_do_list.validators import summary_length, min_words
 
+class Type(models.Model):
     name = models.CharField(max_length=100, default='Task', verbose_name='Type')
 
     def __str__(self):
@@ -13,9 +15,7 @@ class Type(models.Model):
         verbose_name_plural = 'Types'
 
 
-
 class Status(models.Model):
-
     name = models.CharField(max_length=100, default='new', verbose_name='Status')
 
     def __str__(self):
@@ -27,16 +27,24 @@ class Status(models.Model):
         verbose_name_plural = 'Statuses'
 
 
-
-
 class Task(models.Model):
-
-    summary = models.CharField(max_length=100, null=False, unique=True)
-    description = models.TextField(null=True, blank=True)
-    status = models.ForeignKey('to_do_list.Status', on_delete=models.PROTECT)
-    type = models.ForeignKey('to_do_list.Type', null=False, on_delete=models.PROTECT)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    summary = models.CharField(
+        max_length=100,
+        null=False,
+        unique=True,
+        validators=[summary_length],
+        verbose_name='Summary'
+    )
+    description = models.TextField(
+        null=True,
+        blank=True,
+        validators=[min_words],
+        verbose_name='Description'
+    )
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, verbose_name='Status')
+    type = models.ForeignKey(Type, null=False, on_delete=models.PROTECT, verbose_name='Type')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
 
     def __str__(self):
         return self.summary
@@ -45,6 +53,3 @@ class Task(models.Model):
         db_table = 'tasks'
         verbose_name = 'Task'
         verbose_name_plural = 'Tasks'
-
-
-
